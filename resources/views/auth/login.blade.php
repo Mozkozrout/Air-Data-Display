5e4d55f4870ae585c3eeb8cadaf94eceb5bea0d8
Mozkozrout <j4nvelebny@gmail.com>
2024-02-01 23:45:27 +0100

    Template edits

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,10 @@
-@extends('layouts')
+@extends('layout')
 @section('content')
 <main class="login-form">
-    <div class="cotainer">
+    <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-4">
-                <div class="card">
+                <div class="card shadow">
                     <h3 class="card-header text-center">Login</h3>
                     <div class="card-body">
                         <form method="POST" action="{{ route('login.handle') }}">
@@ -23,15 +23,8 @@
                                 <span class="text-danger">{{ $errors->first('password') }}</span>
                                 @endif
                             </div>
-                            <div class="form-group mb-3">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Pamatuj si mě
-                                    </label>
-                                </div>
-                            </div>
                             <div class="d-grid mx-auto">
-                                <button type="submit" class="btn btn-dark btn-block">Přihlásit</button>
+                                <button type="submit" class="btn btn-outline-primary">Přihlásit</button>
                             </div>
                         </form>
                     </div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,9 +5,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
+    <nav class="navbar navbar-dark bg-dark navbar-expand-lg mb-5">
         <div class="container">
-            <a class="navbar-brand mr-auto" href="#">Air Data Rocks! ;D</a>
+            <a class="navbar-brand mr-auto" href="#">Air Data viewer</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -15,11 +15,11 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     @guest
-                    <li class="nav-item">
+                    <li class="nav-item my-lg-0">
                         <a class="nav-link" href="{{ route('login') }}">Login</a>
                     </li>
                     @else
-                    <li class="nav-item">
+                    <li class="nav-item my-lg-0">
                         <a class="nav-link" href="{{ route('logout') }}">Logout</a>
                     </li>
                     @endguest
@@ -30,5 +30,9 @@
     <div class="container mt-5">
         @yield('content')
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
