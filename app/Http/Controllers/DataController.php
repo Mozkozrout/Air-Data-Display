417a12dc4ddae358e5b6aaeaaf68c49da978886f
Mozkozrout <j4nvelebny@gmail.com>
2024-02-03 14:31:15 +0100

    Added function for saving fetched data to DB to DataController

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Data;
 
 class DataController extends Controller
 {
@@ -34,12 +35,13 @@ class DataController extends Controller
                 'verify' => base_path('cacert.pem'),
                 ])
                 ->withQueryParameters([
-                    'access_token' => $token //<--- I would add it using "withToken" or using "withHeaders" but it kept adding it like an array with one item
+                    'access_token' => $token //<--- I would add it using "withToken" or using "withHeaders" but it kept adding it to the request like an array with one element
                 ])
             ->get('{+endpoint}/{page}/{collection}');
 
         if($response->ok()){
             $data = json_decode($response->getBody(), true)['data'];
+            $data = $this->updateDB($data);
             return $data;
         }
 
@@ -47,6 +49,34 @@ class DataController extends Controller
             $error = json_decode($response->getBody(), true);
             return 'Chyba: ' . $error['errors']['0']['message'] . ' Code: ' . $response->getStatusCode();
         }
+    }
+
+    private function updateDB($data){
+        $modelData = [];
+        foreach($data as $item){
+            array_push($modelData,
+            Data::firstOrCreate([
+                'data_id' => $item['id']
+            ],[
+                'sort' => $item['sort'],
+                'user_created' => $item['user_created'],
+                'user_updated' => $item['user_updated'],
+                'date_created' => $item['date_created'],
+                'date_updated' => $item['date_updated'],
+                'HEX' => $item['HEX'],
+                'temperature' => $item['temperature'],
+                'co2' => $item['co2'],
+                'c2ho' => $item['c2ho'],
+                'humidity' => $item['humidity'],
+                'check' => $item['check'],
+                'pm10' => $item['pm10'],
+                'pm25' => $item['pm25'],
+                'tvoc' => $item['tvoc'],
+                'valid' => $item['valid'],
+            ]));
+        }
+
+        return $modelData;
     }
 }
 
